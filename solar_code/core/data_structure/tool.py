1c4455a07a56ed050bbf351599995bb96f3f4306
lxfight <1686540385@qq.com>
2025-03-09 11:59:44 +0800

    1

--- solar_code/core/data_structure/tool.py
+++ solar_code/core/data_structure/tool.py
@@ -6,7 +6,7 @@ class memorandum:
     """备忘录"""
     title: str                  # 备忘标题
     content: str                # 备忘内容
-    mem_type: str               # （单次/循环：每天、每周、每月、每年（生日等）、工作日）
+    memo_type: str               # （单次/循环：每天、每周、每月、每年（生日等）、工作日）
     reminder_time: float        # 提醒时间
     state: str                  # （未完成/已完成/已过期）
     timestamp: float            # 时间戳
