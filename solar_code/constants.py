4ffcea87f4182643fd2c649b015d2571348049c4
lxfight <1686540385@qq.com>
2025-03-09 20:54:32 +0800

    删除constants

--- solar_code/constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))  # 此时是根目录路径
-RESOURCE_DIR = os.path.join(PROJECT_ROOT, "resources")
-ICON_PATH = os.path.join(RESOURCE_DIR, "icon.ico")
-
-print(f"项目根目录：{PROJECT_ROOT}")
-print(f"资源目录：{RESOURCE_DIR}")
-print(f"图标路径：{ICON_PATH}")
